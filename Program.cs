f0323812481458ac7ebc554d5c22b52aa9a777b0
camiladlsa <camilaestherdls@hotmail.com>
2021-02-16 09:47:40 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace INS215L_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ""\\Mac\Home\Downloads\INS215L_No.1_1093141_Camila De Los Santos\INS215L_No.1_1093141_Camila De Los Santos\Laboratorio_1\MyData.mdf""; Integrated Security = True");
+            connection.Open();
+            Console.WriteLine(connection.State);
+
+            {
+                Student Class = new Student()
+                {
+                    ID = 1093141,
+                    Name = "Camila Esther",
+                    LastName = "De Los Santos Arias"
+                };
+                Console.WriteLine("\nID: {0} \nNombres: {1}\nApellidos: {2}", Class.ID, Class.Name, Class.LastName);
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
+    }
+}  
---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INS215L_1
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
